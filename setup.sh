0daf5b635fcf44d0165aee8ae40a9621963108ab
Lohit Jethwani <jethwanilohit@gmail.com>
2023-01-31 16:50:53 +0530

    Initial Setup: Updated repos

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+!/bin/bash
+
+# Get the argument as the replacement string
+replacement=$1
+
+# Find all files recursively from the current directory
+find . -type f -print0 | while read -d $'\0' file
+do
+  # Check if the file has "{dockerHubUsername}" in it
+  if grep -q -a -I "{dockerHubUsername}" "$file"; then
+    # Replace "{dockerHubUsername}" with the argument
+    sed -i "s/{dockerHubUsername}/$replacement/g" "$file"
+  fi
+done
